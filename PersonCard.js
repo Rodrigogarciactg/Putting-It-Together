45cd5fa65477f9312477835f971c0bf73a3ccb03
Rodrigogarciactg <100664773+Rodrigogarciactg@users.noreply.github.com>
2022-05-30 13:33:09 -0700

    Add files via upload

---
+++ PersonCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ageUp: this.props.age,
+    };
+  }
+  render() {
+    const { firstName, lastName, hair } = this.props;
+    return (
+      <div>
+        <h2>
+          {lastName}, {firstName}
+        </h2>
+        <p>Age:{this.state.ageUp}</p>
+        <p>Hair Color: {hair}</p>
+        <button onClick={() => this.setState({ ageUp: this.state.ageUp + 1 })}>
+          Birthday Button for {firstName} {lastName}
+        </button>
+      </div>
+    );
+  }
+}
+export default PersonCard;
